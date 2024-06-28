7549be018b7acc469e5a2a16fb8ece75352eda9a
Robert Ward (PhD School of Computer Sci FT) <rjw207@student.bham.ac.uk>
2024-06-28 17:41:26 +0100

    Reorganised structure

--- customFunctions/opticalProperties/waterMua.m
+++
@@ -1,9 +0,0 @@
-function mua = waterMua(extinctionCoeff)
-%WATERMUA Summary of this function goes here
-%   Detailed explanation goes here
-
-
-    mua = log(10) * extinctionCoeff * 1000 / 18.02;
-
-end
-
